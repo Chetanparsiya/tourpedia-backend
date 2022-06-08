3d8978492ac6a5d9f07d1dcebf186e43353ba4d0
Chetan Parsiya <chetanparsiya207@gmail.com>
2022-06-08 12:59:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(morgan("dev"))
+app.use(express.json({limit:"30mb", extended: true}));
+app.use(express.urlencoded({limit:"30mb", extended: true}));
+
+const port = 5000;
+const MONGO_URL = "mongodb+srv://root:root@123@cluster0.jb1ry.mongodb.net/tour_db?retryWrites=true&w=majority"
+
+//mongodb+srv://root:root@123@cluster0.jb1ry.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(MONGO_URL).then(()=> {
+  app.listen(port, ()=> { console.log(`Server running on port no ${port}`)})
+}).catch(err => {
+  console.log(`${err} didn't connect`)
+})
---
+++ models/user.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+let user = new mongoose.Schema({
+  email: {type:String, required: true},
+  password: {type:String, required: true},
+  firstName: {type:String, required: true},
+  lastName: {type:String, required: false},
+  id: {type: String}
+})
+export default mongoose.model('User', userSchema)
